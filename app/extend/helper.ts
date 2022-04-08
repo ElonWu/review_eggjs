2651224cccf0a36b979c422b346678b84765c91b
吴渊 <wrzmonkey@gmail.com>
2022-04-08 09:41:17 +0800

    feat: 通用的上行参数验证 helper

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,1 +1,18 @@
-module.exports = {};
+import { Context } from 'egg';
+
+module.exports = {
+  validateError(ctx: Context, rule: any, data: any): string | null {
+    try {
+      ctx.validate(rule, data);
+    } catch (err) {
+      const errors: { field: string; message: string }[] = (err as any).errors;
+
+      return errors.reduce(
+        (acc, { field, message }) => acc + `${field} ${message};`,
+        '',
+      );
+    }
+
+    return null;
+  },
+};
